3cf69715504a880d1a0b9afedfc88ccd994d8fe7
marvinorias <oriasmarvinm@gmail.com>
2022-08-10 15:18:27 +0800

    Simple gallery app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+import logo1 from './images/foxhound.png';
+import logo2 from './images/ff7.gif';
+import logo3 from './images/fox.png';
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <h2 className="h2">Gallery</h2>
+      <div className="grid">
+        
+        <img src={logo1} 
+        alt="image1"
+        className="image"/>
+
+        <img src={logo2} 
+        alt="image1"
+        className="image"/>
+        
+        <img src={logo3} 
+        alt="image1"
+        className="image"/>
+      </div>
+    </div>
+  );
+}
